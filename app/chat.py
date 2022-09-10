0791145273d80fb5d4af14115af98323141ae11b
vviveks <vishwavivek0562@outlook.com>
2022-09-10 16:26:53 +0530

    initial commit

---
+++ app/chat.py
@@ -0,0 +1,65 @@
+import random
+import json
+import torch
+from app.model import ChatModel
+from app.nltk_utils import tokenize, stem, bag_of_words
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('app/intents.json', 'r') as f:
+    intents = json.load(f)
+
+FILE = "app/data.pth"
+
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data["all_words"]
+tags = data["tags"]
+model_state = data["model_state"]
+
+model = ChatModel(input_size, hidden_size, output_size)
+model.load_state_dict(model_state)
+
+model.eval()
+
+bot_name = "Ryuk"
+
+def get_response(sentence):
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X)
+
+    output = model(X)
+
+    _ , predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                return random.choice(intent["responses"])
+    else:
+        return "I do not understand..."
+
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
+
+
+
+
